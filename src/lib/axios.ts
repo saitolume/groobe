1e00fb217252c7fe12761fb6ae1e93591920a9f0
Tadao Iseki <saitoeku3@gmail.com>
2020-03-16 19:41:50 +0900

    Fix base url for api client

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,8 @@
 import Axios from 'axios'
 import { env } from '../constants/env'
 
+const { DOMAIN, IS_DEV } = env
+
 export const axios = Axios.create({
-  baseURL: `https://${env.DOMAIN}/api`
+  baseURL: `http${IS_DEV ? '' : 's'}://${DOMAIN}/api`
 })
