ffd4c836a43d506bb2fd3b7661ed672591692ada
Tadao Iseki <saitoeku3@gmail.com>
2020-03-16 19:18:21 +0900

    Add type definition of passport-spotify

---
+++ types/passport-spotify.d.ts
@@ -0,0 +1,26 @@
+import { SpotifyProfile } from '../src/domains/user/model'
+
+declare module 'passport-spotify' {
+  type Options = {
+    clientID: string
+    clientSecret: string
+    callbackURL: string
+  }
+
+  type Verify = (
+    accessToken: string,
+    refreshToken: string,
+    expiresIn: unknown,
+    profile: SpotifyProfile,
+    done: Function
+  ) => void
+
+  export class Strategy {
+    constructor(options: Options, verify: Verify)
+
+    authenticate(
+      req: IncomingMessage,
+      options: { scope: string[]; showDialog: boolean; failureRedirect: string }
+    ): void
+  }
+}
