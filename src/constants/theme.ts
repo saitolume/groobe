7ab3063fba6f45dced74d685db5f26d7ad1c8750
Tadao Iseki <saitoeku3@gmail.com>
2020-03-18 11:55:01 +0900

    Add gray color to theme

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,10 +1,14 @@
 export const theme = {
-  colors: {
-    black: '#222'
+  color: {
+    black: '#222',
+    white: '#fff',
+    gray: '#eee'
   }
 } as const
 
 type AppTheme = typeof theme
+
+export type Color = keyof AppTheme['color']
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
