1a3a1cee82f5f1d3acbd8243635a9d11fd965a39
Tadao Iseki <saitoeku3@gmail.com>
2020-03-18 17:59:18 +0900

    Import service account config on initializing firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,11 @@
 import * as admin from 'firebase-admin'
 import { env } from '../constants/env'
-import serviceAccount from '../../config/serviceAccount.json'
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert(
+      require('../../config/serviceAccount.json') as admin.ServiceAccount
+    ),
     databaseURL: env.DATABSE_URL
   })
 }
